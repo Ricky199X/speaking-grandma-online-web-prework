54869dfda0f49b696fe4de4ad57496469b860b42
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-07 03:18:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,8 +19,7 @@ def speak_to_grandma(string)
     "HUH?! SPEAK UP, SONNY!"
   elsif string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif
-    string == string.upcase
+  else
     "NO, NOT SINCE 1938!"
   end
 end
