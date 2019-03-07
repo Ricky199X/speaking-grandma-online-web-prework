96bd141498dc2c3bc7276d2e8de0bc1676986a80
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-07 03:18:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,8 @@ def speak_to_grandma(string)
     "HUH?! SPEAK UP, SONNY!"
   elsif string == string.capitalize
     "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.swapcase
+    "HUH?! SPEAK UP, SONNY!"
   elsif string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif
